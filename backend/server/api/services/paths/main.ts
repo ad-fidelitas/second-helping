fda8849868483a2a708b2ed0fb05f90834b2b428
Alexandre <alexandre.h.falardeau@gmail.com>
2019-01-26 17:43:56 -0500

    bug fix

---
+++ backend/server/api/services/paths/main.ts
@@ -0,0 +1,11 @@
+import { IPathService } from "../../../interfaces/IPathService";
+
+export const PathService: () => IPathService =
+() => {
+    // private functions here
+
+    // public functions in the return module 
+    return {
+
+    }
+}
